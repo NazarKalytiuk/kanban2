d415e24af8ddcccc19000656b6a279624f2aa389
Nazar Kalytiuk <nazarkalytiuk@gmail.com>
2019-03-19 18:35:06 +0200

    Test paths

--- src/app/core/services/repositories/local-storage-repository.service.spec.ts
+++ src/app/core/services/repositories/local-storage-repository.service.spec.ts
@@ -22,7 +22,6 @@ function initLocalStorageMock() {
 }
 
 describe('LocalStorageRepositoryService', () => {
-
   let service: LocalStorageRepositoryService = null;
   let destroyed$: Subject<boolean> = null;
 
--- src/app/core/services/todo.service.spec.ts
+++ src/app/core/services/todo.service.spec.ts
@@ -3,112 +3,117 @@ import { TestBed } from '@angular/core/testing';
 import { TodoService } from './todo.service';
 import { StorageRepository } from './repositories/storage-repository';
 import { Todo } from '@core/model';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { IndexeddbRepositoryService } from './repositories/indexeddb-repository.service';
 import { LocalStorageRepositoryService } from './repositories/local-storage-repository.service';
+import { flatMap, takeUntil } from 'rxjs/operators';
 
 class MockRepository implements StorageRepository<Todo> {
-  private todo: Todo = {
-    checked: false,
-    title: 'title',
-    id: 1
-  };
+  store: Todo[] = [];
   get(id: number): Observable<Todo> {
-    return of(this.todo);
+    const result = this.store[id];
+    return of(result);
   }
   getAll(): Observable<Todo[]> {
-    return of([this.todo]);
+    return of(this.store);
   }
   add(todo: Todo): Observable<Todo> {
+    this.store[todo.id] = todo;
     return of(todo);
   }
   remove(todo: Todo): Observable<undefined> {
+    delete this.store[todo.id];
     return of(undefined);
   }
   edit(todo: Todo): Observable<Todo> {
+    this.store[todo.id] = todo;
     return of(todo);
   }
 }
 
 describe('TodoService', () => {
+  let destroyed$: Subject<boolean> = null;
+  let service: TodoService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [{provide: IndexeddbRepositoryService, useValue: new MockRepository()},
         {provide: LocalStorageRepositoryService, useValue: new MockRepository()}]
     });
+    destroyed$ = new Subject();
+    service = TestBed.get(TodoService);
+  });
+
+  afterEach(() => {
+    destroyed$.next(true);
+    destroyed$.complete();
   });
 
   it('should be created', () => {
-    const service: TodoService = TestBed.get(TodoService);
     expect(service).toBeTruthy();
   });
 
   it('should add new todo', (done) => {
-    const service: TodoService = TestBed.get(TodoService);
     const todo: Todo = {
       checked: false,
       title: 'title',
       id: 1
     };
-    service.add(todo).subscribe(s => {
-      service.get(todo.id).subscribe(e => {
-        expect(todo).toEqual(e);
-        done();
-      });
+    service.add(todo).pipe(
+      flatMap(e => service.get(e.id)),
+      takeUntil(destroyed$)
+    ).subscribe(e => {
+      expect(todo).toEqual(e);
+      done();
     });
   });
 
   it('should edit todo', (done) => {
-    const service: TodoService = TestBed.get(TodoService);
     const todo: Todo = {
-      checked: false,
-      title: 'title',
-      id: 1
-    };
-    service.add(todo).subscribe(s => {
-      service.edit({...todo, checked: true}).subscribe(r => {
-        service.get(todo.id).subscribe(e => {
-          expect(todo.checked).not.toEqual(e.checked);
-          done();
-        });
-      });
-    });
-  });
-
-  it('should remove todo', (done) => {
-    const service: TodoService = TestBed.get(TodoService);
-    const todo: Todo = {
-      checked: false,
-      title: 'title',
-      id: 1
-    };
-    const todo2: Todo = {
       checked: false,
       title: 'title',
       id: 2
     };
-    service.add(todo).subscribe(s => {
-      service.add(todo2).subscribe(w => {
-        service.remove(todo).subscribe(null, null, () => {
-          service.get(todo.id).subscribe(e => {
-            expect(e).toBeUndefined();
-            done();
-          });
-        });
-      });
+    service.add(todo).pipe(
+      flatMap(e => service.edit({...todo, checked: true})),
+      flatMap(s => service.get(s.id)),
+      takeUntil(destroyed$)
+    ).subscribe(e => {
+      expect(todo.checked).not.toEqual(e.checked);
+      done();
+    });
+  });
+
+  it('should remove todo', (done) => {
+    const todo: Todo = {
+      checked: false,
+      title: 'title',
+      id: 3
+    };
+    service.add(todo).pipe(
+      flatMap(e => service.remove(todo)),
+      flatMap(s => service.get(todo.id)),
+      takeUntil(destroyed$)
+    ).subscribe(e => {
+      expect(e).toBeUndefined();
+      done();
     });
   });
 
   it('should get new todo from storage todo', (done) => {
-    const service: TodoService = TestBed.get(TodoService);
-    service.get(5).subscribe(e => {
+    const todo: Todo = {
+      checked: false,
+      title: 'title',
+      id: 4
+    };
+    service.add(todo).pipe(
+      flatMap(e => service.get(e.id))
+    ).subscribe(e => {
       expect(e).not.toBeUndefined();
       done();
     });
   });
 
   it('should get all todos', (done) => {
-    const service: TodoService = TestBed.get(TodoService);
     service.getAll().subscribe(e => {
       expect(e).not.toBeUndefined();
       done();
